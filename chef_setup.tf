6eacd3e30fb27bc5233b2265e8100f490ca3a620
Eric Gonzales <guardians611@gmail.com>
2020-09-29 08:43:00 -0400

    add quotes around project name

--- chef_setup.tf
+++ chef_setup.tf
@@ -1,7 +1,7 @@
 module "service_accounts" {
   source        = "terraform-google-modules/service-accounts/google"
   version       = "~> 3.0"
-  project_id    = tdd-testing-environment
+  project_id    = "tdd-testing-environment"
   names         = ["chef-inspec"]
   display_name  = "Chef InSpec Service Account"
   description   = "The Service Account used by Chef InSpec for Infrastructure Testing"
