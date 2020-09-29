edaaa2a913091bb3c364534c5cdb7e62986640cd
Eric Gonzales <guardians611@gmail.com>
2020-09-29 09:10:34 -0400

    update name for chef inspec service account

--- chef_setup.tf
+++ chef_setup.tf
@@ -2,7 +2,7 @@ module "service_accounts" {
   source        = "terraform-google-modules/service-accounts/google"
   version       = "~> 3.0"
   project_id    = "tdd-testing-environment"
-  names         = ["chef-inspec"]
+  names         = ["chef-inspec-sa"]
   display_name  = "Chef InSpec Service Account"
   description   = "The Service Account used by Chef InSpec for Infrastructure Testing"
   project_roles = ["tdd-testing-environment=>roles/viewer"]
