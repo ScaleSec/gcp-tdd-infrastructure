85143c47772231909984b4e487a861b21fa39410
Eric Gonzales <guardians611@gmail.com>
2020-09-29 08:41:42 -0400

    build resources in tdd-testing-environment

--- chef_setup.tf
+++ chef_setup.tf
@@ -1,9 +1,9 @@
 module "service_accounts" {
   source        = "terraform-google-modules/service-accounts/google"
   version       = "~> 3.0"
-  project_id    = var.project_id
+  project_id    = tdd-testing-environment
   names         = ["chef-inspec"]
   display_name  = "Chef InSpec Service Account"
   description   = "The Service Account used by Chef InSpec for Infrastructure Testing"
-  project_roles = ["${var.project_id}=>roles/viewer"]
+  project_roles = ["tdd-testing-environment=>roles/viewer"]
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "project_id" {
   description = "The Project ID used for this account"
-  default     = "scalesec-dev"
+  default     = "tdd-testing-environment"
 }
