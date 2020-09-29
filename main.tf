f8a9297695b4ae84bd6283676881958a6ce6aa95
Eric Gonzales <guardians611@gmail.com>
2020-09-29 11:17:42 -0400

    remove unused firewall rune

--- main.tf
+++ main.tf
@@ -52,15 +52,4 @@ resource "google_compute_instance" "scalesec" {
   }
 }
 
-resource "google_compute_firewall" "ssh" {
-  name    = "allow-ssh"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-}
 
