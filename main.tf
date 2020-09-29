198003ec0e55c34d803f1dd911fd4bb5b5923b2c
Eric Gonzales <guardians611@gmail.com>
2020-09-28 20:07:36 -0400

    add labels to test instance

--- main.tf
+++ main.tf
@@ -14,16 +14,22 @@ resource "google_compute_instance" "scalesec" {
   name = "scalesec-test"
   machine_type = "n1-standard-1"
   zone = "us-west1-a"
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
     }
   }
+
   network_interface {
     network = "default"
     access_config {
       // External IP address is default
     }
+  }
+
+  labels = {
+    foo = "bar"
   }
 }
 
