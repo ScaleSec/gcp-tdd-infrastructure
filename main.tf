9ee39762ea8228f08d1ac7d5e057b6a942eac965
Eric Gonzales <guardians611@gmail.com>
2020-09-29 11:20:14 -0400

    firewall rule added

--- main.tf
+++ main.tf
@@ -52,4 +52,15 @@ resource "google_compute_instance" "scalesec" {
   }
 }
 
+resource "google_compute_firewall" "ssh" {
+  name    = "allow-ssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
 
